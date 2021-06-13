b71a499586cde7f645d7aa11f4470cbfd2ef2a09
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2021-06-13 20:45:04 -0300

    menu funcinal

---
+++ public/scripts/menuShow.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    window.document.getElementById('user').classList.add('hidden')
+    window.document.getElementById('menu').classList.remove('hidden')
+}
+
+function closeMenu() {
+    window.document.getElementById('user').classList.remove('hidden')
+    window.document.getElementById('menu').classList.add('hidden')
+}
