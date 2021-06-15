90e6b038da2d095dce1202fa5b15232ff71af5ef
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2021-06-15 10:02:51 -0300

    Create dialog form

---
+++ public/scripts/createShow.js
@@ -0,0 +1,9 @@
+function openCreate() {
+    window.document.getElementById('shadow').classList = ''
+    window.document.getElementById('create').classList = ''
+}
+
+function closeCreate() {
+    window.document.getElementById('shadow').classList = 'hidden'
+    window.document.getElementById('create').classList = 'hidden'
+}
--- public/views/home.php
+++ public/views/home.php
@@ -58,13 +58,13 @@
     <!-- End Header -->
 
     <div class="add">
-        <a href="">
+        <div class="left" onclick="openCreate()">
             <i class="fas fa-plus"></i>
 
             <span>
                 Add new 
             </span>
-        </a>
+        </div>
 
         <div></div> <!-- Futura busca -->
     </div>
@@ -112,6 +112,26 @@
         </div>
     </footer>
 
+    <div id="shadow" class="hidden" onclick="closeCreate()"></div>
+    <div id="create" class="hidden">
+        <div class="top">
+            <h3>Create</h3>
+
+            <div class="close" onclick="closeCreate()">
+                    <i class="fas fa-times-circle btn"></i>
+            </div>
+        </div>
+
+        <form action="" onsubmit="return registerValidate(event)" method="POST">
+            <input type="text" name="name" id="name" placeholder="Nome">
+            <input type="number" name="quantity" id="quantity" placeholder="Quantidade">
+            <input type="number" name="price" id="price" placeholder="Preço">
+            <input type="text" name="type" id="type" placeholder="Tipo">
+            <input type="submit" class="submitBtn" value="Adicionar">
+        </form>
+    </div>
+
     <script src="../scripts/menuShow.js"></script>
+    <script src="../scripts/createShow.js"></script>
 </body>
 </html>
