1f9a6040c27d96840b43375075aecf39e5c1b775
Felipe_Estevanatto <69355764+FelipeEstevanatto@users.noreply.github.com>
2021-06-16 22:25:03 -0300

    Scripts atualizados
    Co-Authored-By: Gabriel Gomes Nicolim <gabriel.gomes.nicolim@gmail.com>

--- public/scripts/modalShow.js
+++ public/scripts/modalShow.js
@@ -8,24 +8,30 @@ function closeCreate() {
     window.document.getElementById('create').classList = 'hidden'
 }
 
-function openEdit() {
+function openEdit(id) {
     window.document.getElementById('shadow').classList = ''
     window.document.getElementById('edit').classList = ''
+    window.document.getElementById('editInput').value = id
 }
 
 function closeEdit() {
     window.document.getElementById('shadow').classList = 'hidden'
     window.document.getElementById('edit').classList = 'hidden'
+
 }
 
-function openExclude(id) {
+function openExclude() {
     window.document.getElementById('shadow').classList = ''
     window.document.getElementById('exclude').classList = ''
-
-    window.location.href += '?exclude=' + id
 }
 
 function closeExclude() {
     window.document.getElementById('shadow').classList = 'hidden'
     window.document.getElementById('exclude').classList = 'hidden'
+}
+
+function openExclude(id) {
+    window.document.getElementById('shadow').classList = ''
+    window.document.getElementById('exclude').classList = ''
+    window.document.getElementById('excludeInput').value = id
 }
