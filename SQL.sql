ea27bdcdd96e2e0c9fd6fc7d5b317d7973cc3c31
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2021-07-03 23:59:02 -0300

    md to sql

--- SQL.sql
+++ SQL.sql
@@ -1,6 +1,4 @@
-<h2>Criar tabelas</h2>
-
-<pre>
+/* Usuários */
 CREATE TABLE usuarios (
 id_user SERIAL PRIMARY KEY NOT NULL,
 nome VARCHAR(40) NOT NULL,
@@ -9,24 +7,24 @@ senha VARCHAR(255) NOT NULL,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 nivelAcesso INT NOT NULL DEFAULT 0
 );
-</pre>
 
-<pre>
+/* Registros */
 CREATE TABLE registros (
-idRegistro BIGSERIAL PRIMARY KEY NOT NULL,
+idRegistro SERIAL PRIMARY KEY NOT NULL,
 nomeProd VARCHAR(100),
 qntProd INT NOT NULL,
 tipoProd VARCHAR(20),
 valorProd DECIMAL(10,2),
 excluido BOOLEAN NOT NULL,
 data_exclusao DATE,
-fk_user INT NOT NULL,
+fk_user BIGINT UNSIGNED NOT NULL,
 FOREIGN KEY (fk_user) REFERENCES usuarios (id_user)
 );
-</pre>
 
-<h2>Dropar tabelas</h2>
+/* Criar tabelas */
 
-<pre>
-DROP TABLE registros, usuarios;
-</pre>
+CREATE DATABASE alice_db;
+
+/* Dropar tabelas */
+
+/* DROP TABLE registros, usuarios; */
